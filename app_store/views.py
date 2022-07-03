24509ffbb16dc9ceb7b3827add134086707bb75d
smaster0517 <>
2022-07-03 13:40:53 -0500

    added search function for products. Viewable in store product page.

--- app_store/views.py
+++ app_store/views.py
@@ -1,20 +1,14 @@
-# from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from app_store.models import Product
-
-# def home(request):
-#     all_products = Product.objects.all()
-#     context = {
-#         'title' : 'Home', 
-#         'all_products' : all_products
-#     }
-#     return render(request, "app_store/home.html", context)
+from functools import reduce
+import operator
+from django.db.models import Q
 
 class FeaturedProductListView(ListView):
     model = Product
     template_name = 'app_store/home.html' # <app>/<model>_<viewtype>.html
     context_object_name = 'all_products'
-    ordering = ['-date_posted'] # date_posted - oldest to newest
+    ordering = ['-date_posted']
 
 class ProductDetailView(DetailView):
     model = Product
@@ -23,5 +17,12 @@ class StoreProductListView(ListView):
     model = Product
     template_name = 'app_store/store_products.html'
     context_object_name = 'all_products'
-    ordering = ['-date_posted']
     paginate_by = 12
+
+    def get_queryset(self):
+        query = self.request.GET.get('search')
+        if query:
+            query = query.split()
+            object_list = self.model.objects.filter(reduce(operator.or_, (Q(title__icontains = search_word) for search_word in query)))
+            return object_list
+        return Product.objects.all().order_by('-date_posted')
