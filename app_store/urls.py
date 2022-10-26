4dbdd0bd95c8ed8a2037b580553e2a380b86c9e8
smaster0517 <>
2022-10-25 21:45:35 -0500

    Added wishlist page,link in user profile. Initiated add and remove items from wishlist.

--- app_store/urls.py
+++ app_store/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import FeaturedProductListView, ProductDetailView, StoreProductListView
+from .views import FeaturedProductListView, ProductDetailView, StoreProductListView, WishListView
 from . import views
 
 urlpatterns = [
     path('about/', views.about, name='store-about'),
     path('',  FeaturedProductListView.as_view(), name='store-home'),
     path('store/',  StoreProductListView.as_view(), name='store-products'),
-    path('product/<int:pk>/',  ProductDetailView.as_view(), name='product-detail')
+    path('product/<int:pk>/',  ProductDetailView.as_view(), name='product-detail'),
+    path('wishlist/',  WishListView.as_view(), name='wishlist-products')
 ]
--- app_store/views.py
+++ app_store/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView, DetailView
-from app_store.models import Product
+from app_store.models import Product, WishlistProduct
 from functools import reduce
 import operator
 from django.db.models import Q
@@ -15,10 +15,26 @@ class FeaturedProductListView(ListView):
     def get_context_data(self, **kwargs):
         context = super(FeaturedProductListView, self).get_context_data(**kwargs)
         context['all_featured_products'] = self.model.objects.filter(featured=True).all().order_by('-date_posted')
+        if self.request.user.is_authenticated:
+            current_user_wishlist_products = WishlistProduct.objects.all().filter(user=self.request.user)
+            current_user_product_id_list = []
+            for product in current_user_wishlist_products:
+                current_user_product_id_list.append(product.product.id)
+            context['current_user_wishlist_products'] = current_user_product_id_list
         return context
 
 class ProductDetailView(DetailView):
     model = Product
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(**kwargs)
+        if self.request.user.is_authenticated:
+            current_user_wishlist_products = WishlistProduct.objects.all().filter(user=self.request.user)
+            current_user_product_id_list = []
+            for product in current_user_wishlist_products:
+                current_user_product_id_list.append(product.product.id)
+            context['current_user_wishlist_products'] = current_user_product_id_list
+        return context
 
 class StoreProductListView(ListView):
     model = Product
@@ -33,3 +49,24 @@ class StoreProductListView(ListView):
             object_list = self.model.objects.filter(reduce(operator.or_, (Q(title__icontains = search_word) for search_word in query)))
             return object_list
         return Product.objects.all().order_by('-date_posted')
+    
+    def get_context_data(self, **kwargs):
+        context = super(StoreProductListView, self).get_context_data(**kwargs)
+        if self.request.user.is_authenticated:
+            current_user_wishlist_products = WishlistProduct.objects.all().filter(user=self.request.user)
+            current_user_product_id_list = []
+            for product in current_user_wishlist_products:
+                current_user_product_id_list.append(product.product.id)
+            context['current_user_wishlist_products'] = current_user_product_id_list
+        return context
+    
+class WishListView(ListView):
+    model = WishlistProduct
+    template_name = 'app_store/user_wishlist.html'
+    context_object_name = 'all_wishlist_products'
+    paginate_by = 20
+
+    def get_query_set(self, **kwargs):
+        query_set = super(WishListView, self).get_context_data(**kwargs)
+        query_set = self.model.objects.all().filter(user=self.request.user).order_by('-date_added')
+        return query_set
