9021d8d1e21d993e219a849fae23a4146b1e6888
smaster0517 <>
2022-11-14 21:03:09 -0600

    Added Django forms to update user profile first_name, last_name, and email.

--- app_users/forms.py
+++ app_users/forms.py
@@ -1,4 +1,3 @@
-from pyexpat import model
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
@@ -9,3 +8,10 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class UpdateProfileForm(forms.ModelForm):
+    email = forms.EmailField
+    
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
--- app_users/views.py
+++ app_users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UpdateProfileForm
 from django.contrib import messages
 from app_store.models import Wishlist
 from django.contrib.auth.decorators import login_required
@@ -20,7 +20,24 @@ def register(request):
 def profile(request):
     wishlist = Wishlist.objects.get(user=request.user)
     products_in_wishlist = wishlist.wishlistproduct_set.order_by('-date_added')[:10] 
+    if request.method == 'POST':
+        form = UpdateProfileForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+
+            user = request.user
+            user.first_name = first_name
+            user.last_name = last_name
+            user.email = email
+            user.save()
+    else:
+        user = request.user
+        form = UpdateProfileForm(instance=user)
+
     context = {
-        'products_in_wishlist': products_in_wishlist
+        'products_in_wishlist': products_in_wishlist,
+        'form': form
     }
     return render(request, "app_users/profile.html", context=context)
